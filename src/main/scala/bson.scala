f6c72a910d6bd206c21ea3ab683fc891228c98ea
Stephane Godbillon <stephane@godbillon.fr>
2012-07-25 00:45:04 +0200

    BSONObjectID generator.

--- src/main/scala/bson.scala
+++ src/main/scala/bson.scala
@@ -126,6 +126,53 @@ case class BSONObjectID(value: Array[Byte]) extends BSONValue {
   def write(buffer: ChannelBuffer) = { buffer writeBytes value; buffer }
 }
 
+object BSONObjectID {
+  import java.net._
+  private val maxCounterValue = 16777216
+  private val increment = new java.util.concurrent.atomic.AtomicInteger(scala.util.Random.nextInt(maxCounterValue))
+
+  private def counter = (increment.getAndIncrement + maxCounterValue) % maxCounterValue
+
+  private val machineId = {
+    val networkInterfacesEnum = NetworkInterface.getNetworkInterfaces
+    val networkInterfaces = scala.collection.JavaConverters.enumerationAsScalaIteratorConverter(networkInterfacesEnum).asScala
+    networkInterfaces.find(_.getHardwareAddress != null)
+      .map(_.getHardwareAddress)
+      .getOrElse(InetAddress.getLocalHost.getHostName.getBytes)
+      .take(3)
+  }
+
+  /** Generates a new BSON ObjectID. */
+  def generate :BSONObjectID = {
+    val timestamp = (System.currentTimeMillis / 1000).toInt
+
+    // n of seconds since epoch. Big endian
+    val id = new Array[Byte](12)
+    id(0) = (timestamp >>> 24).toByte
+    id(1) = (timestamp >> 16 & 0xFF).toByte
+    id(2) = (timestamp >> 8 & 0xFF).toByte
+    id(3) = (timestamp & 0xFF).toByte
+
+    // machine id, 3 first bytes of md5(macadress or hostname)
+    id(4) = machineId(0)
+    id(5) = machineId(1)
+    id(6) = machineId(2)
+
+    // 2 bytes of the pid or thread id. Thread id in our case. Low endian
+    val threadId = Thread.currentThread.getId.toInt
+    id(7) = (threadId & 0xFF).toByte
+    id(8) = (threadId >> 8 & 0xFF).toByte
+
+    // 3 bytes of counter sequence, which start is randomized. Big endian
+    val c = counter
+    id(9) = (c >> 16 & 0xFF).toByte
+    id(10) = (c >> 8 & 0xFF).toByte
+    id(11) = (c & 0xFF).toByte
+
+    BSONObjectID(id)
+  }
+}
+
 /** BSON boolean value */
 case class BSONBoolean(value: Boolean) extends BSONValue {
   val code = 0x08
