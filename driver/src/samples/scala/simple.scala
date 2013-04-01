476341779fd8d83e6c305e32167131226db91b90
Stephane Godbillon <stephane@godbillon.fr>
2013-04-01 23:53:50 +0200

    Update simple use cases sample

--- driver/src/samples/scala/simple.scala
+++ driver/src/samples/scala/simple.scala
@@ -3,73 +3,86 @@ package samples
 import play.api.libs.iteratee.Iteratee
 import reactivemongo.api._
 import reactivemongo.bson._
-import reactivemongo.bson.handlers.DefaultBSONHandlers._
 import reactivemongo.core.commands._
-import scala.util.{Failure, Success}
+import scala.util.{ Failure, Success }
 
 object SimpleUseCases {
   import scala.concurrent.ExecutionContext.Implicits.global // TODO create own ExecutionContext
 
-  val connection = MongoConnection( List( "localhost:27016" ) )
+  // gets an instance of the driver
+  // (creates an actor system)
+  val driver = new MongoDriver
+  /* Gets a connection using this MongoDriver instance.
+   * The connection creates a pool of connections.
+   * *** USE WITH CAUTION ***
+   * Both ReactiveMongo class and MongoConnection class should be instantiated one time in your application
+   * (each time you make a ReactiveMongo instance, you create an actor system; each time you create a MongoConnection
+   * instance, you create a pool of connections).
+   */
+  val connection = driver.connection(List("localhost:27017"))
+
+  // Gets a reference to the database "plugin"
   val db = connection("plugin")
+
+  // Gets a reference to the collection "acoll"
+  // By default, you get a BSONCollection.
   val collection = db("acoll")
 
   def listDocs() = {
     // select only the documents which field 'firstName' equals 'Jack'
-    val query = BSONDocument("firstName" -> BSONString("Jack"))
+    val query = BSONDocument("firstName" -> "Jack")
     // select only the field 'lastName'
     val filter = BSONDocument(
-      "lastName" -> BSONInteger(1),
-      "_id" -> BSONInteger(0)
-    )
+      "lastName" -> 1,
+      "_id" -> 0)
 
-    // get a Cursor[TraversableBSONDocument]
-    val cursor = collection.find(query, filter)
+    // get a Cursor[BSONDocument]
+    val cursor = collection.find(query, filter).cursor[BSONDocument]
     // let's enumerate this cursor and print a readable representation of each document in the response
     cursor.enumerate.apply(Iteratee.foreach { doc =>
       println("found document: " + BSONDocument.pretty(doc))
     })
 
     // or, the same with getting a list
-    val cursor2 = collection.find(query, filter)
+    val cursor2 = collection.find(query, filter).cursor[BSONDocument]
     val futurelist = cursor2.toList
     futurelist.onSuccess {
       case list =>
-        val names = list.map(_.getAs[BSONString]("lastName").get.value)
+        val names = list.map(_.getAs[String]("lastName").get)
         println("got names: " + names)
     }
   }
 
   def count() = {
     // select only the documents which company name equals 'Zenexity'
-    val query = BSONDocument("company.name" -> BSONString("Zenexity"))
+    val query = BSONDocument("company.name" -> "Zenexity")
     val futureCount = db.command(
       Count(
         // run this command on the given collection
         collection.name,
         // ... with the query we wrote above
-        Some(query)
-      )
-    )
+        Some(query)))
     futureCount.map { count =>
       println("found " + count + " documents which company name is 'Zenexity'")
     }
   }
 
   def querybuilder() = {
-    val query = QueryBuilder().
-      // select only the documents which company name equals 'Zenexity'
-      query( BSONDocument("company.name" -> BSONString("Zenexity")) ).
-      // sort by lastName
-      sort("lastName" -> SortOrder.Ascending).
-      // retrieve only lastName and firstName
-      projection( BSONDocument(
-        "lastName" -> BSONInteger(1),
-        "firstName" -> BSONInteger(1),
-        "_id" -> BSONInteger(0)) )
-    // get a Cursor[DefaultBSONIterator]
-    val cursor = collection.find(query)
-    // get a future list
+    val cursor =
+      collection.
+        // select only the documents which company name equals 'Zenexity'
+        find(BSONDocument("company.name" -> "Zenexity")).
+        // sort by lastName
+        sort(BSONDocument("lastName" -> 1)).
+        // retrieve only lastName and firstName
+        projection(BSONDocument(
+          "lastName" -> 1,
+          "firstName" -> 1,
+          "_id" -> 0)).
+        // get a Cursor[BSONDocument]
+        cursor[BSONDocument]
+
+    // get a future list of BSONDocuments
     val futurelist = cursor.toList
     futurelist.onSuccess {
       case list => println(list.map(doc => BSONDocument.pretty(doc)))
@@ -78,37 +91,33 @@ object SimpleUseCases {
 
   def insert() = {
     val document = BSONDocument(
-      "firstName" -> BSONString("Stephane"),
-      "lastName" -> BSONString("Godbillon"),
-      "age" -> BSONInteger(28),
+      "firstName" -> "Stephane",
+      "lastName" -> "Godbillon",
+      "age" -> 29,
       "company" -> BSONDocument(
-        "name" -> BSONString("Zenexity"),
-        "address" -> BSONString("56 rue Saint Lazare 75009 Paris")
-      )
-    )
+        "name" -> "Zenexity",
+        "address" -> "56 rue Saint Lazare 75009 Paris"))
 
-    val future = collection.insert(document, GetLastError())
+    val future = collection.insert(document)
     future.onComplete {
       case Failure(e) => throw e
       case Success(lastError) => {
-        println("successfully inserted document: "  + lastError)
+        println("successfully inserted document: " + lastError)
       }
     }
   }
 
   def insertThenCount() = {
     val document = BSONDocument(
-      "firstName" -> BSONString("Stephane"),
-      "lastName" -> BSONString("Godbillon"),
-      "age" -> BSONInteger(28),
+      "firstName" -> "Stephane",
+      "lastName" -> "Godbillon",
+      "age" -> 28,
       "company" -> BSONDocument(
-        "name" -> BSONString("Zenexity"),
-        "address" -> BSONString("56 rue Saint Lazare 75009 Paris")
-      )
-    )
+        "name" -> "Zenexity",
+        "address" -> "56 rue Saint Lazare 75009 Paris"))
 
     // get the future result of the insertion
-    val futureInsert = collection.insert(document, GetLastError())
+    val futureInsert = collection.insert(document)
 
     // when the insertion is successfully done, we send the count command
     val futureInsertThenCount = futureInsert.flatMap(lasterror => {
@@ -116,8 +125,7 @@ object SimpleUseCases {
       val count = Count(
         collection.name,
         Some(BSONDocument(
-          "company.name" -> BSONString("Zenexity")
-        )))
+          "company.name" -> "Zenexity")))
       // get the future count
       db.command(count)
     })
@@ -131,15 +139,14 @@ object SimpleUseCases {
   }
 
   def update() = {
-    val selector = BSONDocument("name" -> BSONString("Jack"))
+    val selector = BSONDocument("name" -> "Jack")
 
     val modifier = BSONDocument(
       "$set" -> BSONDocument(
-        "lastName" ->BSONString("London"),
-        "firstName" -> BSONString("Jack")),
+        "lastName" -> "London",
+        "firstName" -> "Jack"),
       "$unset" -> BSONDocument(
-        "name" -> BSONInteger(1))
-    )
+        "name" -> 1))
 
     // get a future update
     val futureUpdate = collection.update(selector, modifier, GetLastError())
@@ -147,10 +154,10 @@ object SimpleUseCases {
     // get a cursor of documents that have lastName: "London" and firstName: "Jack". Our updated document should be included.
     val futureCursor = futureUpdate.map { lastError =>
       val updatedDocumentSelector = BSONDocument(
-        "lastName" -> BSONString("London"),
-        "firstName" -> BSONString("Jack"))
+        "lastName" -> "London",
+        "firstName" -> "Jack")
 
-      collection.find(updatedDocumentSelector)
+      collection.find(updatedDocumentSelector).cursor[BSONDocument]
     }
 
     // let's enumerate this cursor and print a readable representation of each document in the response
@@ -163,7 +170,7 @@ object SimpleUseCases {
   def remove() = {
     // let's remove all documents that have company.name = Zenexity
     val selector = BSONDocument(
-      "company.name" -> BSONString("Zenexity"))
+      "company.name" -> "Zenexity")
 
     val futureRemove = collection.remove(selector, GetLastError(), false)
 
@@ -178,10 +185,10 @@ object SimpleUseCases {
   // finds all documents with lastName = Godbillon and replace lastName with GODBILLON
   def findAndModify() = {
     val selector = BSONDocument(
-      "lastName" -> BSONString("Godbillon"))
+      "lastName" -> "Godbillon")
 
     val modifier = BSONDocument(
-      "$set" -> BSONDocument("lastName" -> BSONString("GODBILLON")))
+      "$set" -> BSONDocument("lastName" -> "GODBILLON"))
 
     val command = FindAndModify(
       collection.name,
@@ -198,8 +205,7 @@ object SimpleUseCases {
           // get a BSONIterator (lazy BSON parser) of this document
           // stringify it with DefaultBSONIterator.pretty
           BSONDocument.pretty(doc)
-        })
-      )
+        }))
     }
   }
 }
