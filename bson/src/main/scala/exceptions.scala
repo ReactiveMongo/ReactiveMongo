5f1f31ab89d23efdc70b796c4ab2e7a963a29597
Eugene Platonov <jozic@ua.fm>
2013-11-14 09:42:39 -0500

    DocumentKeyNotFound message now says "NOT found"

--- bson/src/main/scala/exceptions.scala
+++ bson/src/main/scala/exceptions.scala
@@ -16,5 +16,5 @@
 package reactivemongo.bson.exceptions
 
 case class DocumentKeyNotFound(name: String) extends Exception {
-  override def getMessage = s"The key '$name' could be found in this document or array"
+  override def getMessage = s"The key '$name' could not be found in this document or array"
 }
