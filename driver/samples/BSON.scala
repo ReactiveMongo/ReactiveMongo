ff507354c4247218ffe2258b3738569f9ee34b5b
Stephane Godbillon <stephane@godbillon.fr>
2013-11-18 23:47:41 +0100

    Map de/serialization examples

--- driver/samples/BSON.scala
+++ driver/samples/BSON.scala
@@ -326,4 +326,28 @@ object BSONSamples {
       println(s"someClassValue == someClassValueFromBSON ? ${someClassValue equals someClassValueFromBSON}")
     }
   }
+
+  // Map examples
+  implicit def MapReader[V](implicit vr: BSONDocumentReader[V]): BSONDocumentReader[Map[String, V]] = new BSONDocumentReader[Map[String, V]] {
+    def read(bson: BSONDocument): Map[String, V] = {
+      val elements = bson.elements.map { tuple =>
+        // assume that all values in the document are BSONDocuments
+        tuple._1 -> vr.read(tuple._2.seeAsTry[BSONDocument].get)
+      }
+      elements.toMap
+    }
+  }
+
+  implicit def MapWriter[V](implicit vw: BSONDocumentWriter[V]): BSONDocumentWriter[Map[String, V]] = new BSONDocumentWriter[Map[String, V]] {
+    def write(map: Map[String, V]): BSONDocument = {
+      val elements = map.toStream.map { tuple =>
+        tuple._1 -> vw.write(tuple._2)
+      }
+      BSONDocument(elements)
+    }
+  }
+
+  val map = Map("artist1" -> neilYoung)
+  val docFromMap: BSONDocument = BSON.writeDocument(map)
+  val mapFromDoc: Map[String, Artist] = BSON.readDocument[Map[String, Artist]](docFromMap)
 }
