d141408856b0feec49da11898d79bc285b288337
Andrey Neverov <neverov.and@gmail.com>
2014-01-15 16:07:25 +0400

    Tests cleanup.

--- driver/src/test/scala/GridfsSpec.scala
+++ driver/src/test/scala/GridfsSpec.scala
@@ -1,12 +1,10 @@
 import org.specs2.mutable._
 import play.api.libs.iteratee.Enumerator
 import reactivemongo.api._
-import reactivemongo.api.gridfs.{DefaultFileToSave, GridFS}
+import reactivemongo.api.gridfs.{ReadFile, DefaultFileToSave, GridFS}
 import reactivemongo.api.gridfs.Implicits._
 import reactivemongo.bson._
-import reactivemongo.core.commands.Count
 import scala.concurrent._
-import scala.util.Failure
 import reactivemongo.api.gridfs
 import scala.concurrent.duration._
 
@@ -17,24 +15,27 @@ class GridfsSpec extends Specification {
 
   lazy val gfs = GridFS(db)
 
-  lazy val file = DefaultFileToSave("somefile", Some("application/file"), Some(1387201380000l), id = BSONObjectID.generate)
+  lazy val file = DefaultFileToSave("somefile", Some("application/file"))
   lazy val fileContent = Enumerator((1 to 100).view.map(_.toByte).toArray)
 
   "ReactiveMongo" should {
 
     "store a file in gridfs" in {
-      Await.result(gfs.save(fileContent, file), Duration(3, SECONDS)).filename mustEqual "somefile"
+      val actual = Await.result(gfs.save(fileContent, file), Duration(3, SECONDS))
+      actual.filename mustEqual "somefile"
     }
 
     "find this file in gridfs" in {
-      Await.result(gfs.find(BSONDocument("filename" -> "somefile")).headOption, Duration(3, SECONDS)) match {
-        case None => failure("file not found")
-        case Some(f) => success
-      }
+      val futureFile = gfs.find(BSONDocument("filename" -> "somefile")).collect[List]()
+      val actual = Await.result(futureFile, Duration(3, SECONDS)).head
+      (actual.filename mustEqual file.filename) and
+      (actual.uploadDate must beSome) and
+      (actual.contentType mustEqual file.contentType)
     }
 
     "delete this file from gridfs" in {
-      Await.result(gfs.remove(file.id), Duration(3, SECONDS)).n mustEqual 1
+      val actual = Await.result(gfs.remove(file.id), Duration(3, SECONDS))
+      actual.n mustEqual 1
     }
   }
 }
