e8b7249b989469bdab83284a6b18d32bb2422b21
Stephane Godbillon <stephane@godbillon.fr>
2014-10-09 15:54:47 +0200

    Release 0.10.5.0.akka23

--- project/ReactiveMongo.scala
+++ project/ReactiveMongo.scala
@@ -2,7 +2,7 @@ import sbt._
 import sbt.Keys._
 
 object BuildSettings {
-  val buildVersion = "0.11.0-SNAPSHOT"
+  val buildVersion = "0.10.5.0.akka23"
 
   val filter = { (ms: Seq[(File, String)]) =>
     ms filter {
