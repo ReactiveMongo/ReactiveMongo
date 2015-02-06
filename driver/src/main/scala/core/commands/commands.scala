204a4cc12c59db899461fae50a2cc551a7e32652
Olivier Samyn <olivier@26lights.com>
2015-02-06 11:11:36 +0100

    Avoid None.get when deleting an index.

--- driver/src/main/scala/core/commands/commands.scala
+++ driver/src/main/scala/core/commands/commands.scala
@@ -592,7 +592,7 @@ case class DeleteIndex(
 
   object ResultMaker extends BSONCommandResultMaker[Int] {
     def apply(document: BSONDocument) =
-      CommandError.checkOk(document, Some("deleteIndexes")).toLeft(document.getAs[BSONDouble]("nIndexesWas").map(_.value.toInt).get) }
+      CommandError.checkOk(document, Some("deleteIndexes")).toLeft(document.getAs[BSONInteger]("nIndexesWas").map(_.value.toInt).get) }
 }
 
 /** eval command */
