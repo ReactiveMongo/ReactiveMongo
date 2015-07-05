9c7e82f17ddb6ce1bf647d58b0adb3138afc08fe
cchantep <chantepie@altern.org>
2015-07-05 15:24:25 +0200

    Easier findAndModify

--- driver/src/main/scala/api/commands/bson/findandmodify.scala
+++ driver/src/main/scala/api/commands/bson/findandmodify.scala
@@ -1,16 +1,21 @@
 package reactivemongo.api.commands.bson
 
 import reactivemongo.api.BSONSerializationPack
-import reactivemongo.api.commands._
-import reactivemongo.bson._
+import reactivemongo.api.commands.{
+  FindAndModifyCommand,
+  ResolvedCollectionCommand
+}
+import reactivemongo.bson.{ BSONDocument, BSONDocumentWriter, BSONValue }
 
-object BSONFindAndModifyCommand extends FindAndModifyCommand[BSONSerializationPack.type] {
+object BSONFindAndModifyCommand
+    extends FindAndModifyCommand[BSONSerializationPack.type] {
   val pack: BSONSerializationPack.type = BSONSerializationPack
 }
 
 object BSONFindAndModifyImplicits {
   import BSONFindAndModifyCommand._
   implicit object FindAndModifyResultReader extends DealingWithGenericCommandErrorsReader[FindAndModifyResult] {
+
     def readResult(result: BSONDocument): FindAndModifyResult =
       FindAndModifyResult(
         result.getAs[BSONDocument]("lastErrorObject").map { doc =>
@@ -22,20 +27,23 @@ object BSONFindAndModifyImplicits {
         },
         result.getAs[BSONDocument]("value"))
   }
-  implicit object FindAndModifyWriter extends BSONDocumentWriter[ResolvedCollectionCommand[FindAndModify]] {
-    def write(command: ResolvedCollectionCommand[FindAndModify]): BSONDocument =
+
+  implicit object FindAndModifyWriter
+      extends BSONDocumentWriter[ResolvedCollectionCommand[FindAndModify]] {
+
+    def write(cmd: ResolvedCollectionCommand[FindAndModify]): BSONDocument =
       BSONDocument(
-        "findAndModify" -> command.collection,
-        "query" -> command.command.query,
-        "sort" -> command.command.sort,
-        "fields" -> command.command.fields,
-        "upsert" -> (if (command.command.upsert) Some(true) else None)) ++ (command.command.modify match {
-          case Update(document, fetchNewObject) =>
+        "findAndModify" -> cmd.collection,
+        "query" -> cmd.command.query,
+        "sort" -> cmd.command.sort,
+        "fields" -> cmd.command.fields) ++
+        (cmd.command.modify match {
+          case Update(document, fetchNewObject, upsert) =>
             BSONDocument(
+              "upsert" -> upsert,
               "update" -> document,
               "new" -> fetchNewObject)
-          case Remove =>
-            BSONDocument("remove" -> true)
+          case Remove => BSONDocument("remove" -> true)
         })
   }
 }
