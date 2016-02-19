c052b8be4d20009008f5508225855aa0318a1f8d
cchantep <chantepie@altern.org>
2016-02-19 12:19:05 +0100

    Minor release 0.11.10

--- project/ReactiveMongo.scala
+++ project/ReactiveMongo.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 import scala.language.postfixOps
 
 object BuildSettings {
-  val buildVersion = "0.12.0-SNAPSHOT"
+  val buildVersion = "0.11.10" //"0.12.0-SNAPSHOT"
 
   val filter = { (ms: Seq[(File, String)]) =>
     ms filter {
