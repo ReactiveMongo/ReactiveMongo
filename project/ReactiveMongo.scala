e9b569be2f2dd6e9bda137631509ea8840fd9943
Francis <fdoumet@gmail.com>
2016-03-04 14:58:20 -0800

    Agenda build

--- project/ReactiveMongo.scala
+++ project/ReactiveMongo.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 import scala.language.postfixOps
 
 object BuildSettings {
-  val buildVersion = "0.12.0-SNAPSHOT"
+  val buildVersion = "0.12.0-SNAPSHOT-ADENDA"
 
   val filter = { (ms: Seq[(File, String)]) =>
     ms filter {
