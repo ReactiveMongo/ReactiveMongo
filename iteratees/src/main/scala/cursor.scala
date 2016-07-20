294315e117759731ccbf9891bbc24d8efa059a27
cchantep <chantepie@altern.org>
2016-07-20 22:54:37 +0200

    Bump to 0.12-RC0

--- iteratees/src/main/scala/cursor.scala
+++ iteratees/src/main/scala/cursor.scala
@@ -65,26 +65,30 @@ class PlayIterateesCursorImpl[T](val wrappee: Cursor[T])
   override def enumerator(maxDocs: Int = Int.MaxValue, err: ErrorHandler[Unit] = FailOnError[Unit]())(implicit ctx: ExecutionContext): Enumerator[T] =
     Concurrent.unicast[T] { chan =>
       wrappee.foldWhile({}, maxDocs)(
-        (_, res) => Cont(chan push res), errorHandler(chan, err)).
+        (_, res) => Cont(chan push res), errorHandler(chan, err)
+      ).
         onComplete { case _ => chan.eofAndEnd() }
     }
 
   override def bulkEnumerator(maxDocs: Int = Int.MaxValue, err: ErrorHandler[Unit] = FailOnError[Unit]())(implicit ctx: ExecutionContext): Enumerator[Iterator[T]] = Concurrent.unicast[Iterator[T]] { chan =>
     wrappee.foldBulks({}, maxDocs)(
-      (_, bulk) => Cont(chan push bulk), errorHandler(chan, err)).
+      (_, bulk) => Cont(chan push bulk), errorHandler(chan, err)
+    ).
       onComplete { case _ => chan.eofAndEnd() }
   }
 
   override def responseEnumerator(maxDocs: Int = Int.MaxValue, err: ErrorHandler[Unit] = FailOnError[Unit]())(implicit ctx: ExecutionContext): Enumerator[Response] = Concurrent.unicast[Response] { chan =>
     wrappee.foldResponses({}, maxDocs)(
-      (_, resp) => Cont(chan push resp), errorHandler(chan, err)).
+      (_, resp) => Cont(chan push resp), errorHandler(chan, err)
+    ).
       onComplete { case _ => chan.eofAndEnd() }
   }
 
 }
 
 class PlayIterateesFlattenedCursor[T](
-  val cursor: Future[PlayIterateesCursor[T]])
+  val cursor: Future[PlayIterateesCursor[T]]
+)
     extends FlattenedCursor[T](cursor) with PlayIterateesCursor[T] {
 
   override def enumerator(maxDocs: Int = Int.MaxValue, err: ErrorHandler[Unit] = FailOnError[Unit]())(implicit ctx: ExecutionContext): Enumerator[T] = Enumerator.flatten(cursor.map(_.enumerator(maxDocs, err)))
