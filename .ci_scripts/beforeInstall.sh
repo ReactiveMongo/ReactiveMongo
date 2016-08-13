0a5ddf86dcb68fce588827ffc7c0d1751df7f8cc
cchantep <chantepie@altern.org>
2016-08-13 18:11:56 +0200

    Setup Circle CI

--- .ci_scripts/beforeInstall.sh
+++ .ci_scripts/beforeInstall.sh
@@ -100,61 +100,13 @@ fi
 echo "# MongoDB Configuration:"
 cat /tmp/mongod.conf
 
-numactl --interleave=all mongod -f /tmp/mongod.conf --port 27018 --fork
+# Print version information
+MV=`mongod --version 2>/dev/null | head -n 1`
 
-MONGOD_PID=`ps -o pid,comm -u $USER | grep 'mongod$' | awk '{ printf("%s\n", $1); }'`
-
-if [ "x$MONGOD_PID" = "x" ]; then
-    echo -e "\nERROR: Fails to start the custom 'mongod' instance" > /dev/stderr
-
-    mongod --version
-    PID=`ps -o pid,comm -u $USER | grep 'mongod$' | awk '{ printf("%s\n", $1); }'`
-
-    if [ ! "x$PID" = "x" ]; then
-        pid -p $PID
-    else
-        echo "ERROR: MongoDB process not found" > /dev/stderr
-    fi
-
-    tail -n 100 /tmp/mongod.log
-
-    exit 1
-fi
-
-# Check MongoDB connection
-MONGOSHELL_OPTS=""
-
-if [ "$MONGO_PROFILE" = "ssl" -a ! "$MONGO_VER" = "2_6" ]; then
-    MONGOSHELL_OPTS="$MONGOSHELL_OPTS --ssl --sslAllowInvalidCertificates"
-fi
-
-MONGOSHELL_OPTS="$MONGOSHELL_OPTS --eval"
-MONGODB_NAME=`mongo "$PRIMARY_HOST/FOO" $MONGOSHELL_OPTS 'db.getName()' 2>/dev/null | tail -n 1`
-
-if [ ! "x$MONGODB_NAME" = "xFOO" ]; then
-    echo -n "\nERROR: Fails to connect using the MongoShell\n"
-    mongo "$PRIMARY_HOST/FOO" $MONGOSHELL_OPTS 'db.getName()'
-    tail -n 100 /tmp/mongod.log
-    exit 2
-fi
-
-# Check MongoDB runtime
-
-echo -n "- security: "
-mongo "$PRIMARY_HOST/FOO" $MONGOSHELL_OPTS 'var s=db.serverStatus();var x=s["security"];(!x)?"_DISABLED_":x["SSLServerSubjectName"];' 2>/dev/null | tail -n 1
-
-echo -n "- storage engine: "
-mongo "$PRIMARY_HOST/FOO" $MONGOSHELL_OPTS 'var s=db.serverStatus();JSON.stringify(s["storageEngine"]);' 2>/dev/null | grep '"name"' | cut -d '"' -f 4
-
-if [ "$MONGO_PROFILE" = "rs" ]; then
-    mongo "$PRIMARY_HOST" $MONGOSHELL_OPTS "rs.initiate({\"_id\":\"testrs0\",\"version\":1,\"members\":[{\"_id\":0,\"host\":\"$PRIMARY_HOST\"}]});" || (
-        echo "ERROR: Fails to setup the ReplicaSet" > /dev/stderr
-        false
-    )
-fi
+echo -n "INFO: Installed MongoDB ($MV): "
+echo "$MV" | sed -e 's/.* v//'
 
 # Export environment for integration tests
-
 cat > /tmp/integration-env.sh <<EOF
 PATH="$PATH"
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
