899e453a2d1e8ce3f58f0efd2777b30e5cec63b7
cchantep <chantepie@altern.org>
2018-08-17 22:51:46 +0200

    Improve backward compatibility about WriteConcern

--- project/Common.scala
+++ project/Common.scala
@@ -18,7 +18,7 @@ object Common {
 
   val closeableObject = SettingKey[String]("class name of a closeable object")
 
-  val settings = Defaults.coreDefaultSettings ++ baseSettings ++ Seq(
+  val settings = Defaults.coreDefaultSettings ++ baseSettings ++ Compiler.settings ++ Seq(
     scalaVersion := "2.12.6",
     crossScalaVersions := Seq("2.10.7", scalaCompatVer, scalaVersion.value),
     crossVersion := CrossVersion.binary,
@@ -34,59 +34,10 @@ object Common {
 
       Seq((sourceDirectory in Compile).value / jdir)
     },
-    scalacOptions ++= Seq(
-      "-encoding", "UTF-8",
-      "-unchecked",
-      "-deprecation",
-      "-feature",
-      //"-Xfatal-warnings",
-      "-Xlint",
-      "-Ywarn-numeric-widen",
-      "-Ywarn-dead-code",
-      "-Ywarn-value-discard",
-      "-g:vars"
-    ),
-    scalacOptions in Compile ++= {
-      if (!scalaVersion.value.startsWith("2.11.")) Nil
-      else Seq(
-        "-Yconst-opt",
-        "-Yclosure-elim",
-        "-Ydead-code",
-        "-Yopt:_"
-      )
-    },
-    scalacOptions in Compile ++= {
-      if (scalaVersion.value startsWith "2.10.") Nil
-      else Seq(
-        "-Ywarn-infer-any",
-        "-Ywarn-unused",
-        "-Ywarn-unused-import",
-        "-Xlint:missing-interpolator"
-      )
-    },
-    scalacOptions in Compile ++= {
-      if (!scalaVersion.value.startsWith("2.12.")) Seq("-target:jvm-1.6")
-      else Seq("-target:jvm-1.8")
-    },
-    scalacOptions in (Compile, console) ~= {
-      _.filterNot { opt => opt.startsWith("-X") || opt.startsWith("-Y") }
-    },
-    scalacOptions in (Test, console) ~= {
-      _.filterNot { opt => opt.startsWith("-X") || opt.startsWith("-Y") }
-    },
-    scalacOptions in (Test, console) += "-Yrepl-class-based",
     scalacOptions in (Compile, doc) ++= Seq("-unchecked", "-deprecation",
       /*"-diagrams", */"-implicits", "-skip-packages", "samples"),
     scalacOptions in (Compile, doc) ++= Opts.doc.title("ReactiveMongo API"),
     scalacOptions in (Compile, doc) ++= Opts.doc.version(Release.major.value),
-    scalacOptions in Compile := {
-      val opts = (scalacOptions in Compile).value
-
-      if (scalaVersion.value != "2.10.7") opts
-      else {
-        opts.filter(_ != "-Ywarn-unused-import")
-      }
-    },
     mappings in (Compile, packageBin) ~= filter,
     mappings in (Compile, packageSrc) ~= filter,
     mappings in (Compile, packageDoc) ~= filter,
