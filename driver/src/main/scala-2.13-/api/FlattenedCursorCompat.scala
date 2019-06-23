ae29b624f2c3c7cf927cc77e0314eed6861566cc
Cédric Chantepie <cchantep@users.noreply.github.com>
2019-06-23 19:52:57 +0200

    Close #826 & Fix deprecation warning in scala 2.13 macro (#829)

--- driver/src/main/scala-2.13-/api/FlattenedCursorCompat.scala
+++ driver/src/main/scala-2.13-/api/FlattenedCursorCompat.scala
@@ -7,6 +7,6 @@ import scala.concurrent.{ ExecutionContext, Future }
 
 private[api] trait FlattenedCursorCompat[T] { _: FlattenedCursor[T] =>
 
-  final def collect[M[_]](maxDocs: Int, err: Cursor.ErrorHandler[M[T]])(implicit cbf: CanBuildFrom[M[_], T, M[T]], ec: ExecutionContext): Future[M[T]] = cursor.flatMap(_.collect[M](maxDocs, err))
+  def collect[M[_]](maxDocs: Int, err: Cursor.ErrorHandler[M[T]])(implicit cbf: CanBuildFrom[M[_], T, M[T]], ec: ExecutionContext): Future[M[T]] = cursor.flatMap(_.collect[M](maxDocs, err))
 
 }
