8608ed043ddc93f27aed979bef331e5ebc499280
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-07-04 21:02:14 +0200

    Update netty-handler, ... to 4.1.37.Final (#834)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Resolvers {
 }
 
 object Dependencies {
-  val netty = "4.1.36.Final"
+  val netty = "4.1.37.Final"
 
   val akka = Def.setting[Seq[ModuleID]] {
     val ver = sys.env.get("AKKA_VERSION").getOrElse {
