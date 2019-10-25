7e17d7f06330fc06db2ecef388366b6648ac7a6b
Cédric Chantepie <cchantep@users.noreply.github.com>
2019-10-25 16:31:03 +0200

    Refactor internal BSON serialization (#884)

--- driver/src/main/scala/api/commands/CollStats.scala
+++ driver/src/main/scala/api/commands/CollStats.scala
@@ -1,8 +1,60 @@
 package reactivemongo.api.commands
 
+import reactivemongo.api.SerializationPack
+
 @deprecated("Internal: will be made private", "0.16.0")
 case class CollStats(scale: Option[Int] = None)
   extends CollectionCommand with CommandWithResult[CollStatsResult]
+
+private[api] object CollStats {
+  def writer[P <: SerializationPack](pack: P): pack.Writer[ResolvedCollectionCommand[CollStats]] = {
+    val builder = pack.newBuilder
+
+    pack.writer[ResolvedCollectionCommand[CollStats]] { cmd =>
+      val elms = Seq.newBuilder[pack.ElementProducer]
+
+      elms += builder.elementProducer(
+        "collStats", builder.string(cmd.collection))
+
+      cmd.command.scale.foreach { scale =>
+        elms += builder.elementProducer("scale", builder.int(scale))
+      }
+
+      builder.document(elms.result())
+    }
+  }
+
+  def reader[P <: SerializationPack](pack: P): pack.Reader[CollStatsResult] = {
+    val decoder = pack.newDecoder
+
+    pack.reader[CollStatsResult] { doc =>
+      (for {
+        ns <- decoder.string(doc, "ns")
+        count <- decoder.int(doc, "count")
+        sz <- decoder.double(doc, "size")
+        avg = decoder.double(doc, "avgObjSize")
+        ss <- decoder.double(doc, "storageSize")
+        ne = decoder.int(doc, "numExtents")
+        ni <- decoder.int(doc, "nindexes")
+        le = decoder.int(doc, "lastExtentSize")
+        pf = decoder.double(doc, "paddingFactor")
+        sf = decoder.int(doc, "systemFlags")
+        uf = decoder.int(doc, "userFlags")
+        ti <- decoder.int(doc, "totalIndexSize")
+        is <- decoder.child(doc, "indexSizes").map { d =>
+          decoder.names(d).toList.flatMap { nme =>
+            decoder.int(d, nme).map(nme -> _)
+          }
+        }
+        cp = decoder.booleanLike(doc, "capped").getOrElse(false)
+        mx = decoder.long(doc, "max")
+        ms = decoder.double(doc, "maxSize")
+      } yield CollStatsResult(
+        ns, count, sz, avg, ss, ne, ni, le, pf, sf,
+        uf, ti, is, cp, mx, ms)).get
+    }
+  }
+}
 
 /**
  * Various information about a collection.
