732af66c5b8f87f29422a5c6e0c6aace3da358a4
Cédric Chantepie <cchantep@users.noreply.github.com>
2019-11-17 20:26:35 +0100

    Fix duplicate buffer trait

--- driver/src/main/scala/core/protocol/protocol.scala
+++ driver/src/main/scala/core/protocol/protocol.scala
@@ -24,19 +24,6 @@ import reactivemongo.api.commands.GetLastError
 
 import reactivemongo.api.ReadPreference
 
-/**
- * A constructor of T instances from a [[http://netty.io/4.1/api/io/netty/buffer/ByteBuf.html ByteBuf]].
- *
- * @tparam T type which instances can be constructed with this.
- */
-trait ChannelBufferReadable[T] {
-  /** Makes an instance of T from the data from the given buffer. */
-  def readFrom(buffer: ByteBuf): T
-
-  /** @see readFrom */
-  def apply(buffer: ByteBuf): T = readFrom(buffer)
-}
-
 // concrete classes
 
 /**
