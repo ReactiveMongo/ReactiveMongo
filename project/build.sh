44f3b29cdc4f8b0f8b7ff48c435930f925256f15
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-01-03 02:59:26 +0100

    Minor release 0.20.0

--- project/build.sh
+++ project/build.sh
@@ -9,8 +9,6 @@ cd "$DIR/.."
 SBT_CMDS="+clean +makePom +packageBin +packageSrc +packageDoc"
 
 sbt $SBT_OPTS $SBT_CMDS
-TMP=`mktemp`
-tar -cvf "$TMP" `find . -path '*/target/*' -type f \( -name '*.jar' -or -name '*.pom' \) -print`
 
 export REACTIVEMONGO_SHADED=false
 sbt $SBT_OPTS $SBT_CMDS
