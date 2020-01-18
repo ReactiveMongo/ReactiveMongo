88587573d637b831a32dec8c276d8a5411efbc58
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-01-18 23:40:40 +0100

    Add ReadConcern snapshot (#932)

--- driver/src/main/scala/api/ReadConcern.scala
+++ driver/src/main/scala/api/ReadConcern.scala
@@ -1,7 +1,22 @@
 package reactivemongo.api
 
 /**
- * The [[https://docs.mongodb.com/manual/reference/read-concern/ Read Concern]] allows to control the consistency and isolation used to read data from replica sets.
+ * The [[https://docs.mongodb.com/manual/reference/read-concern/ Read Concern]]
+ * allows to control the consistency and isolation used to read data
+ * from replica sets.
+ *
+ * {{{
+ * import scala.concurrent.ExecutionContext
+ *
+ * import reactivemongo.api.ReadConcern
+ * import reactivemongo.api.bson.BSONDocument
+ * import reactivemongo.api.bson.collection.BSONCollection
+ *
+ * def queryBuilderWithReadConcern(coll: BSONCollection) =
+ *   coll.find(BSONDocument.empty).
+ *   readConcern(ReadConcern.Available)
+ *
+ * }}}
  */
 sealed trait ReadConcern {
   /** The read concern level */
@@ -18,14 +33,34 @@ sealed trait ReadConcern {
 }
 
 object ReadConcern {
-  object Available extends ReadConcern { val level = "available" }
-
-  /** Requires `--enableMajorityReadConcern` */
-  object Majority extends ReadConcern { val level = "majority" }
-
+  /**
+   * [[https://docs.mongodb.com/manual/reference/read-concern-local/#readconcern.%22local%22 Read Concern 'local']]
+   */
   object Local extends ReadConcern { val level = "local" }
 
+  /**
+   * [[https://docs.mongodb.com/manual/reference/read-concern-available/#readconcern.%22available%22 Read Concern 'available']]
+   * @since MongoDB 3.6
+   */
+  object Available extends ReadConcern { val level = "available" }
+
+  /**
+   * [[https://docs.mongodb.com/manual/reference/read-concern-majority/#readconcern.%22majority%22 Read Concern 'majority']]
+   * @since MongoDB 3.6
+   */
+  object Majority extends ReadConcern { val level = "majority" }
+
+  /**
+   * [[https://docs.mongodb.com/manual/reference/read-concern-linearizable/#readconcern.%22linearizable%22 Read Concern 'linearizable']]
+   * @since MongoDB 3.4
+   */
   object Linearizable extends ReadConcern { val level = "linearizable" }
+
+  /**
+   * [[https://docs.mongodb.com/manual/reference/read-concern-snapshot/#readconcern.%22snapshot%22 Read Concern 'snapshot']]
+   * @since MongoDB 3.4
+   */
+  object Snapshot extends ReadConcern { val level = "snapshot" }
 
   /** Local */
   @inline private[api] def default: ReadConcern = Local
@@ -34,6 +69,7 @@ object ReadConcern {
     case Available.level    => Some(Available)
     case Majority.level     => Some(Majority)
     case Linearizable.level => Some(Linearizable)
+    case Snapshot.level     => Some(Snapshot)
     case _                  => Some(Local)
   }
 }
