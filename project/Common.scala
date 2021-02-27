ede760e4e4941b91f86bb9e2b148ba82f3bd1a41
Scala Steward <me@scala-steward.org>
2021-02-27 01:48:20 +0100

    Update scala-library to 2.12.13

--- project/Common.scala
+++ project/Common.scala
@@ -51,7 +51,7 @@ object Common extends AutoPlugin {
     "Use ReactiveMongo-Shaded (see system property 'reactivemongo.shaded')")
 
   override def projectSettings = Defaults.coreDefaultSettings ++ baseSettings ++ Compiler.settings ++ Seq(
-    scalaVersion := "2.12.12",
+    scalaVersion := "2.12.13",
     crossScalaVersions := Seq(scala211, scalaVersion.value, scala213),
     crossVersion := CrossVersion.binary,
     useShaded := sys.env.get("REACTIVEMONGO_SHADED").fold(true)(_.toBoolean),
