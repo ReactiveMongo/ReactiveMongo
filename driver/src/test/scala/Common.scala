141a92af26a469df0810a139dbb6d86724478140
Cédric Chantepie <cchantep@users.noreply.github.com>
2021-03-23 00:18:46 +0100

    Update Common.scala

--- driver/src/test/scala/Common.scala
+++ driver/src/test/scala/Common.scala
@@ -48,7 +48,7 @@ object Common extends CommonAuth {
 
     if (sys.env.get("MONGO_MINOR") contains "4.4.4") {
       logger.warn("Increase timeout due to MongoDB 4.4 performance regression")
-      nominalValue * 9L
+      nominalValue * 10L
     } else {
       nominalValue
     }
