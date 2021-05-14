e80c7b739e5b88ab0f0a53c2e693f443083110eb
Jacek Sokol <jacek@scalabs.pl>
2021-05-14 23:16:39 +0200

    Fix idle connection disposal (#1037)
    Fix idle connection disposal
    
    Co-authored-by: Jacek Sokół <jsokol@virtuslab.com>
    Co-authored-by: Cédric Chantepie <cchantep@users.noreply.github.com>

--- driver/src/main/scala/core/netty/ChannelFactory.scala
+++ driver/src/main/scala/core/netty/ChannelFactory.scala
@@ -2,6 +2,8 @@ package reactivemongo.core.netty
 
 import java.lang.{ Boolean => JBool }
 
+import java.util.concurrent.TimeUnit
+
 import scala.util.{ Failure, Success, Try }
 
 import scala.concurrent.Promise
@@ -19,6 +21,8 @@ import reactivemongo.io.netty.channel.{
 }, ChannelOption.{ CONNECT_TIMEOUT_MILLIS, SO_KEEPALIVE, TCP_NODELAY }
 
 import reactivemongo.io.netty.channel.ChannelInitializer
+
+import reactivemongo.io.netty.handler.timeout.IdleStateHandler
 
 import akka.actor.ActorRef
 
@@ -114,22 +118,20 @@ private[reactivemongo] final class ChannelFactory(
 
     val pipeline = channel.pipeline
 
+    val idleTimeMS = options.maxIdleTimeMS.toLong
+    pipeline.addLast("idleState", new IdleStateHandler(idleTimeMS, idleTimeMS, 0, TimeUnit.MILLISECONDS))
+
     if (options.sslEnabled) {
-      val sslEng = reactivemongo.core.SSL.
-        createEngine(sslContext, host, port)
-
-      val sslHandler =
-        new reactivemongo.io.netty.handler.ssl.SslHandler(sslEng, false /* TLS */ )
-
-      pipeline.addFirst("ssl", sslHandler)
+      val sslEng = reactivemongo.core.SSL.createEngine(sslContext, host, port)
+      val sslHandler = new reactivemongo.io.netty.handler.ssl.SslHandler(sslEng, false /* TLS */ )
+      pipeline.addLast("ssl", sslHandler)
     }
 
-    val mongoHandler = new MongoHandler(
-      supervisor, connection, receiver, options.maxIdleTimeMS.toLong)
-
     pipeline.addLast(
-      new ResponseFrameDecoder(), new ResponseDecoder(),
-      new RequestEncoder(), mongoHandler)
+      new ResponseFrameDecoder(),
+      new ResponseDecoder(),
+      new RequestEncoder(),
+      new MongoHandler(supervisor, connection, receiver))
 
     trace(s"Netty channel configuration:\n- connectTimeoutMS: ${options.connectTimeoutMS}\n- maxIdleTimeMS: ${options.maxIdleTimeMS}ms\n- tcpNoDelay: ${options.tcpNoDelay}\n- keepAlive: ${options.keepAlive}\n- sslEnabled: ${options.sslEnabled}\n- keyStore: ${options.keyStore.fold("None")(_.toString)}")
   }
