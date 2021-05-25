3aa35d5e4d3cc8955024839ae1e37a8fa0c183f2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-25 09:30:44 +0200

    Update scala-library to 2.13.6 (#1043)
    Co-authored-by: Cédric Chantepie <cchantep@users.noreply.github.com>

--- project/Common.scala
+++ project/Common.scala
@@ -32,7 +32,7 @@ object Common extends AutoPlugin {
   private val java8 = scala.util.Properties.isJavaAtLeast("1.8")
 
   val scala211 = "2.11.12"
-  val scala213 = "2.13.4"
+  val scala213 = "2.13.6"
 
   def majorVersion = {
     val Major = """([0-9]+)\.([0-9]+)\..*""".r
