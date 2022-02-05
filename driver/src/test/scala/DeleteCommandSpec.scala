04405f4e6a872f39001e9dcdf0908f1c6e1dc07d
Cédric Chantepie <cchantep@users.noreply.github.com>
2022-02-05 23:41:20 +0100

    Fix writeConcern for transaction delete (#1121)

--- driver/src/test/scala/DeleteCommandSpec.scala
+++ driver/src/test/scala/DeleteCommandSpec.scala
@@ -1,6 +1,7 @@
 package reactivemongo
 
-import reactivemongo.api.bson.{ BSONArray, BSONDocument }
+import reactivemongo.api.{ NodeSetSession, SessionTransaction }
+import reactivemongo.api.bson.{ BSONArray, BSONBinary, BSONDocument }
 import reactivemongo.api.commands.{ DeleteCommand, ResolvedCollectionCommand }
 import reactivemongo.api.{ PackSupport, Session, WriteConcern }
 
@@ -31,12 +32,56 @@ final class DeleteCommandSpec extends org.specs2.mutable.Specification {
           _q = BSONDocument("_id" -> 1),
           _limit = 1,
           _collation = None)
+
         val bson = BSONDocument(
           "q" -> BSONDocument("_id" -> 1),
           "limit" -> 1)
 
         withDelete(cmd)(element :: Nil) { delete =>
-          cmd.pack.serialize(delete, cmd.deleteWriter) must_=== (base ++ writeConcern ++ deletes(bson :: Nil))
+          cmd.pack.serialize(delete, cmd.deleteWriter) must_=== (
+            base ++ writeConcern ++ deletes(bson :: Nil))
+        }
+      }
+
+      lazy val session = new NodeSetSession(java.util.UUID.randomUUID())
+
+      lazy val lsid = BSONDocument(
+        "lsid" -> BSONDocument(
+          "id" -> BSONBinary(session.lsid)))
+
+      "with session" in {
+        val cmd = new Command(Some(session))
+
+        val element = new cmd.DeleteElement(
+          _q = BSONDocument("_id" -> 1),
+          _limit = 1,
+          _collation = None)
+
+        val bson = BSONDocument(
+          "q" -> BSONDocument("_id" -> 1),
+          "limit" -> 1)
+
+        withDelete(cmd)(element :: Nil) { delete =>
+          val write = cmd.pack.serialize(_: cmd.DeleteCmd, cmd.deleteWriter)
+          val dels = deletes(bson :: Nil)
+
+          // w/o transaction started
+          write(delete) must_=== (base ++ lsid ++ writeConcern ++ dels) and {
+            session.startTransaction(WriteConcern.Default, None).
+              aka("transaction") must beSuccessfulTry[(SessionTransaction, Boolean)].which { _ =>
+                // w/ transaction started
+
+                write(delete) must_=== (base ++ lsid ++ BSONDocument(
+                  "txnNumber" -> 1L,
+                  "startTransaction" -> true, // as first command in tx
+                  "autocommit" -> false) ++ dels)
+              }
+          } and {
+            // w/o 'startTransaction' flag after first command in tx
+
+            write(delete) must_=== (base ++ lsid ++ BSONDocument(
+              "txnNumber" -> 1L, "autocommit" -> false) ++ dels)
+          }
         }
       }
     }
--- version.sbt
+++ version.sbt
@@ -1,7 +1,7 @@
 ThisBuild / dynverVTagPrefix := false
 
 ThisBuild / version := {
-  val Stable = """([0-9]+)\.([0-9]+)\.([0-9]+)""".r
+  val Stable = """([0-9]+)\.([0-9]+)\.([0-9]+)(|-RC[0-9]+)""".r
 
   (ThisBuild / dynverGitDescribeOutput).value match {
     case Some(descr) => {
@@ -9,10 +9,24 @@ ThisBuild / version := {
         (ThisBuild / previousStableVersion).value match {
           case Some(previousVer) => {
             val current = (for {
-              Seq(maj, min, patch) <- Stable.unapplySeq(previousVer)
+              Seq(maj, min, patch, rc) <- Stable.unapplySeq(previousVer)
               nextPatch <- scala.util.Try(patch.toInt).map(_ + 1).toOption
+              nextRc = {
+                if (rc startsWith "-RC") {
+                  scala.util.Try(rc.stripPrefix("-RC").toInt).
+                    map(_ + 1).toOption
+                } else {
+                  Option.empty[Int]
+                }
+              }
             } yield {
-              s"${maj}.${min}.${nextPatch}-SNAPSHOT"
+              nextRc match {
+                case Some(nrc) =>
+                  s"${maj}.${min}.${patch}-RC${nrc}-SNAPSHOT"
+
+                case _ =>
+                  s"${maj}.${min}.${nextPatch}-SNAPSHOT"
+              }
             }).getOrElse {
               println("Fails to determine qualified snapshot version")
               previousVer
