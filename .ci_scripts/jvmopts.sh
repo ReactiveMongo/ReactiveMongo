68c9f4066a3e4c525e44ee22c3893f18602e7f3e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-11-23 18:24:10 +0100

    Update scala3-library to 3.2.1 (#1179)
    * Update scala3-library to 3.2.1
    
    Co-authored-by: Cédric Chantepie <cchantep@users.noreply.github.com>

--- .ci_scripts/jvmopts.sh
+++ .ci_scripts/jvmopts.sh
@@ -1,18 +1,8 @@
 JVM_MAX_MEM="1G"
 
-JAVA_MAJOR=`java -version 2>&1 | head -n 1 | cut -d '"' -f 2 | sed -e 's/\.[0-9a-zA-Z_]*$//'`
-
-if [ "v$JAVA_MAJOR" = "v10.0" -o "v$JAVA_MAJOR" = "v11.0" ]; then
-  JVM_OPTS="-XX:+UseContainerSupport -XX:MaxRAMPercentage=70"
-else
-  JVM_MAX_MEM="1632M" # 1760M"
-  JVM_OPTS="-Xmx$JVM_MAX_MEM -XX:ReservedCodeCacheSize=192m"
-fi
-
-#JVM_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
+JVM_MAX_MEM="1632M" # 1760M"
+JVM_OPTS="-Xmx$JVM_MAX_MEM -XX:ReservedCodeCacheSize=192m"
 
 if [ ! `uname` = "Darwin" ]; then
   export _JAVA_OPTIONS="$JVM_OPTS"
 fi
-
-#export SBT_OPTS="$SBT_OPTS $JVM_OPTS"
