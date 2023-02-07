a2571e5892b0d0f8d999eaa3ce211db007893e61
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-07 23:18:53 +0100

    Update scala3-library to 3.2.2 (#1196)
    * Update scala3-library to 3.2.2
    
    * Update config.yml
    
    ---------
    
    Co-authored-by: Cédric Chantepie <cchantep@users.noreply.github.com>

--- project/Common.scala
+++ project/Common.scala
@@ -31,7 +31,7 @@ object Common extends AutoPlugin {
 
   val scala211 = "2.11.12"
   val scala213 = "2.13.8"
-  val scala31 = "3.2.1" // CI uses 3.1.2-RC1-bin-20220113-8d28d94-NIGHTLY"
+  val scala31 = "3.2.2" // CI uses 3.1.2-RC1-bin-20220113-8d28d94-NIGHTLY"
 
   def majorVersion = {
     val Major = """([0-9]+)\.([0-9]+)\..*""".r
