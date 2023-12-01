bd878f6e34c6f6a5603ed4ac3bf6fe262951ac60
Scala Steward <scala_steward@virtuslab.com>
2023-12-01 20:25:46 +0000

    Update logback-classic to 1.2.13

--- project/Driver.scala
+++ project/Driver.scala
@@ -77,7 +77,7 @@ final class Driver(core: Project, actorModule: Project) {
           ("dnsjava" % "dnsjava" % "3.5.2").exclude("org.slf4j", "*"),
           commonsCodec,
           specs.value,
-          "ch.qos.logback" % "logback-classic" % "1.2.12" % Test
+          "ch.qos.logback" % "logback-classic" % "1.2.13" % Test
         ) ++ logApi,
         mimaBinaryIssueFilters ++= {
           import com.typesafe.tools.mima.core._
