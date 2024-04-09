ac113b3876410f82d1f16813487707b7386e92dd
Nicolas Stucki <nicolas.stucki@gmail.com>
2024-04-09 10:26:31 +0200

    [scala-3] Upgrade to Scala 3.3.3
    Fixes #1251

--- project/Common.scala
+++ project/Common.scala
@@ -34,7 +34,7 @@ object Common extends AutoPlugin {
   val scala211 = "2.11.12"
   val scala212 = "2.12.18"
   val scala213 = "2.13.8"
-  val scala3 = "3.2.2" // CI uses 3.1.2-RC1-bin-20220113-8d28d94-NIGHTLY"
+  val scala3 = "3.3.3"
 
   def majorVersion = {
     val Major = """([0-9]+)\.([0-9]+)\..*""".r
