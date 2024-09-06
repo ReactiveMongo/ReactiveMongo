54df51ba6fb8c8093228d84e4e3b5c11fe9380e8
Scala Steward <scala_steward@virtuslab.com>
2024-09-06 17:23:11 +0000

    Update scala-library to 2.12.20

--- project/Common.scala
+++ project/Common.scala
@@ -32,7 +32,7 @@ object Common extends AutoPlugin {
   private val java8 = scala.util.Properties.isJavaAtLeast("1.8")
 
   val scala211 = "2.11.12"
-  val scala212 = "2.12.19"
+  val scala212 = "2.12.20"
   val scala213 = "2.13.14"
   val scala3 = "3.4.2"
 
