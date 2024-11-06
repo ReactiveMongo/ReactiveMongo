cce8affea438f08e08cc4cf695424332b0be971b
Scala Steward <scala_steward@virtuslab.com>
2024-11-06 00:24:07 +0000

    Update scala3-library to 3.4.3

--- project/Common.scala
+++ project/Common.scala
@@ -34,7 +34,7 @@ object Common extends AutoPlugin {
   val scala211 = "2.11.12"
   val scala212 = "2.12.20"
   val scala213 = "2.13.15"
-  val scala3 = "3.4.2"
+  val scala3 = "3.4.3"
 
   def majorVersion = {
     val Major = """([0-9]+)\.([0-9]+)\..*""".r
