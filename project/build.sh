3ce97e648a446bb7448553a4cc5b9087e111ee5b
Cédric Chantepie <cchantep@users.noreply.github.com>
2025-02-12 17:50:56 +0100

    Release 1.1.0-RC15

--- project/build.sh
+++ project/build.sh
@@ -6,9 +6,9 @@ DIR=`dirname $0 | sed -e "s|^./|$PWD/|"`
 
 cd "$DIR/.."
 
-SBT_CMDS="+clean +makePom +packageBin +packageSrc +packageDoc"
+SBT_CMDS="+makePom +packageBin +packageSrc +packageDoc"
 
-sbt $SBT_OPTS $SBT_CMDS
+sbt $SBT_OPTS +clean $SBT_CMDS
 
 ACTOR_MODULE=pekko sbt $SBT_OPTS "project ReactiveMongo" $SBT_CMDS
 
