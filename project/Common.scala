fbca2557ef9f710ffb122dc279fcd453eba4d1f4
Scala Steward <scala_steward@virtuslab.com>
2025-11-26 22:07:46 +0000

    Update scala-library to 2.13.18

--- project/Common.scala
+++ project/Common.scala
@@ -43,7 +43,7 @@ object Common extends AutoPlugin {
 
   val scala211 = "2.11.12"
   val scala212 = "2.12.20"
-  val scala213 = "2.13.17"
+  val scala213 = "2.13.18"
   val scala3Lts = "3.3.7"
 
   def majorVersion = {
